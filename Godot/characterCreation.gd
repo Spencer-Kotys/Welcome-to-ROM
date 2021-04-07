8035e372d26c14096cefc32d028cf295d1a784ba
Spencer Kotys <58829765+Spencer-Kotys@users.noreply.github.com>
2021-04-06 22:47:31 -0400

    Adding script
    Creating script for scene and connecting buttons

---
+++ Godot/characterCreation.gd
@@ -0,0 +1,28 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Male_pressed():
+	pass # Replace with function body.
+
+
+func _on_Female_pressed():
+	pass # Replace with function body.
+
+
+func _on_Continue_pressed():
+	pass # Replace with function body.
