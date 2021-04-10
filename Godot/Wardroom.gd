24204247f37a176f5639d369db3e6456d248b223
valtroid <hbmeisner@gmail.com>
2021-04-10 15:34:29 -0400

    fixed global audio

--- Godot/Wardroom.gd
+++ Godot/Wardroom.gd
@@ -2,12 +2,15 @@ extends Control
 
 onready var video = get_node("WardroomScene")
 
-
+func _ready():
+	GlobalAudio.stop()
 
 func _on_Button_pressed():
 	video.stop()
+	GlobalAudio.play("res://music/Naoya-Sakamata-Dissociation.ogg")
 	get_tree().change_scene("res://LevelMockup.tscn")
 
 
 func _on_WardroomScene_finished():
 	get_tree().change_scene("res://LevelMockup.tscn")
+	GlobalAudio.play("res://music/Naoya-Sakamata-Dissociation.ogg")
