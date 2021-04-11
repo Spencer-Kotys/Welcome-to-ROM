8c3b578ccd605091b107dc3fbe7b22fdf2b545fa
Spencer Kotys <58829765+Spencer-Kotys@users.noreply.github.com>
2021-04-11 00:16:35 -0400

    Adding Continue Button
    Go back to start screen

--- Godot/Lose.gd
+++ Godot/Lose.gd
@@ -1,16 +1,10 @@
 extends Control
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	globalAudio.play("res://music/I_ll-Remember-You-Jeremy-Blake.ogg")
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_Continue_pressed():
+	globalAudio.stop()
+	get_tree().change_scene("res://startScreen.tscn")
