b1c5a748278b7741b2059f03dda9817bfa5bd70a
Spencer Kotys <58829765+Spencer-Kotys@users.noreply.github.com>
2021-04-13 18:04:04 -0400

    Changing Defaults in Global

--- Godot/global.gd
+++ Godot/global.gd
@@ -8,10 +8,10 @@ var playerName = null # stores player's name
 var playerGender = null # stores player's gender
 var day = "1" # stores day value
 var morale = 50 # stores morale value
-var has_covid = null # true or false for if the player has covid
+var has_covid = false # true or false for if the player has covid
 var bagged = null
-var time_start = null
-var time_end = null
+var time_start = 0
+var time_end = 0
 
 # percent_chance would be an integer between 1 and 100
 # so if you wanted a 5% chance of getting covid enter '5' into the function
