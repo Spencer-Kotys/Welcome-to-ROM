af251c3108be06c3733d2da4fd9dc9ab7e4809a2
Spencer Kotys <58829765+Spencer-Kotys@users.noreply.github.com>
2021-04-13 17:52:38 -0400

    Adding day check

--- Godot/Sleeping.gd
+++ Godot/Sleeping.gd
@@ -4,6 +4,9 @@ extends Control
 func _ready():
 	var day = int(global.day)
 	day = day + 1
-	global.day = str(day)
-	yield(get_tree().create_timer(1.0), "timeout")
-	get_tree().change_scene("res://Room_No_Roommate.tscn")
+	if (14 == day):
+		print("You Win")
+	else:
+		global.day = str(day)
+		yield(get_tree().create_timer(1.0), "timeout")
+		get_tree().change_scene("res://Room_No_Roommate.tscn")
