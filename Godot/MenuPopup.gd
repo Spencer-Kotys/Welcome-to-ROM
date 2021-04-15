a5ec51ed4128c6b653d7da621908002f27760f44
Alex Hwang <alexander.y.hwang@uscga.edu>
2021-04-15 16:05:16 -0400

    Pause Menu

--- Godot/MenuPopup.gd
+++ Godot/MenuPopup.gd
@@ -2,17 +2,22 @@ extends Popup
 
 func _process(delta):
 	if Input.is_action_pressed("pause"):
-		get_tree().paused = true
-		print("hello")
+		if get_tree().paused == true: #when esc is pressed, game is still paused...
+			get_tree().paused = false
+			hide()
+			print("hello")
+		else:
+			get_tree().paused = true
 		popup()
-
-
-func _on_Resume_toggled(button_pressed):
-	get_tree().paused = false
 
 func _on_MainMenu_pressed():
 	get_tree().change_scene("res://startScreen.tscn")
 	get_tree().paused = false
+	hide()
 
 func _on_Quit_pressed():
 	get_tree().quit()
+
+func _on_Resume_pressed():
+	get_tree().paused = false
+	hide()
