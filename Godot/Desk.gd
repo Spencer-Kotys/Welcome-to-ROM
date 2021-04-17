f34df37e2cbac560495939837a94ebfd7ea2a376
Spencer Kotys <58829765+Spencer-Kotys@users.noreply.github.com>
2021-04-17 15:38:28 -0400

    Testing Desk with Bed Dialog

--- Godot/Desk.gd
+++ Godot/Desk.gd
@@ -2,3 +2,20 @@ extends Popup
 
 func _on_MainCharacter_init_desk_dialogue():
 	print("Dialogue")
+	popup()
+
+	var dialog = load("res://addons/dialogs/Dialog.tscn").instance()
+	dialog.dialog_script = [
+	{
+		'dialogue_start': 0
+	},
+	{
+		'question': 'Would you like to go to bed?',
+		'options': [{ 'label': 'Yes', 'value': '0'},
+					{ 'label': 'No', 'value': '1'}],
+		'variable': 'sleep'
+	},
+	{
+		'dialogue_end': 0
+	}]
+	add_child(dialog)
