578aebab7c1a9e6f3becba830ae63cb4e43ecd2c
valtroid <hbmeisner@gmail.com>
2021-04-23 11:29:17 -0400

    cadet 1 has a ton of dialogue now that varies with the day!
    also deleted green boy ;(

--- Godot/Roommate.gd
+++ Godot/Roommate.gd
@@ -5,5 +5,5 @@ func _on_MainCharacter_init_roommate_dialogue():
 	popup()
 
 	var dialog = load("res://addons/dialogs/Dialog.tscn").instance()
-	dialog.dialog_script = global.read_JSON("res://assets/Dialogue Files/Roommate_1.json")
+	dialog.dialog_script = global.read_JSON("res://assets/Dialogue Files/Roommate 1.json")
 	add_child(dialog)
