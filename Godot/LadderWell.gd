6eb94a5a5d6fdf066c5c279f6aeb84070ac38bfb
Spencer Kotys <58829765+Spencer-Kotys@users.noreply.github.com>
2021-04-25 23:37:58 -0400

    Connecting Ladderwell Buttons

--- Godot/LadderWell.gd
+++ Godot/LadderWell.gd
@@ -15,3 +15,32 @@ func _on_Wardroom_pressed():
 func _on_Formation_pressed():
 	get_tree().change_scene("res://Formation.tscn")
 	global.covidChance(1)
+
+
+func _on_Chapel_pressed():
+	pass # Replace with function body.
+
+
+func _on_Colors_pressed():
+	pass # Replace with function body.
+
+
+func _on_CrownPark_pressed():
+	pass # Replace with function body.
+
+
+func _on_DryDock_pressed():
+	pass # Replace with function body.
+
+
+func _on_Gym_pressed():
+	pass # Replace with function body.
+
+
+func _on_Pool_pressed():
+	pass # Replace with function body.
+
+
+func _on_Walk_pressed():
+	pass # Replace with function body.
+
