18194124ef560f7ac04a15a192fb33e1022b57e9
murphya451 <71462425+murphya451@users.noreply.github.com>
2021-04-30 10:56:42 -0400

    randomize dialogue

--- Godot/Roommate.gd
+++ Godot/Roommate.gd
@@ -5,5 +5,15 @@ func _on_MainCharacter_init_roommate_dialogue():
 	popup()
 
 	var dialog = load("res://addons/dialogs/Dialog.tscn").instance()
-	dialog.dialog_script = global.read_JSON("res://assets/Dialogue Files/Roommate 1.json")
+	
+	
+	if global.day == '1' || global.day == '4' || global.day == '9' || global.day == '13':
+		dialog.dialog_script = global.read_JSON("res://assets/Dialogue Files/Roommate 1.json")
+	elif global.day == '2' || global.day == '7' || global.day == '11':
+		dialog.dialog_script = global.read_JSON("res://assets/Dialogue Files/Roommate 2.json")
+	elif global.day == '5' || global.day == '6' || global.day == '8':
+		dialog.dialog_script = global.read_JSON("res://assets/Dialogue Files/Roommate 4.json")
+	elif global.day == '3' || global.day == '10' || global.day == '12':
+		dialog.dialog_script = global.read_JSON("res://assets/Dialogue Files/Roommate 6.json")
+		
 	add_child(dialog)
