46009eb21c1e033b95733b46603e1595d2672a37
valtroid <hbmeisner@gmail.com>
2021-05-01 22:29:39 -0400

    fixed bug with getting bagged

--- Godot/bagged.gd
+++ Godot/bagged.gd
@@ -2,6 +2,12 @@ extends Control
 
 
 func _on_Button_pressed():
-	global.bagged = false
-	global.time = "06:00"
-	get_tree().change_scene("res://Spawn 3.tscn")
+	var day = int(global.day)
+	if (6 < int(global.time)/100):
+		day = day + 1
+	
+	globalTasks.morning_routine_over = false
+	global.day = str(day)
+	global.time = "06:00" # reset time
+	yield(get_tree().create_timer(1.0), "timeout")
+	get_tree().change_scene("res://Room_With_Roommate.tscn")
